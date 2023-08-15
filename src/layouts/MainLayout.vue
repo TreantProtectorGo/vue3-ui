bc7f9c4e674808023c993358e476613b2cb40399
TreantProtectorGo <wing.ws.lee@gmail.com>
2023-08-15 15:13:42 +0800

    1

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-layout view="hHh lpR fFf" >
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+    
+
+    <q-footer >
+      <q-btn-group spread >
+        <q-btn icon="home" label="Home" no-caps stack color="white" text-color="black" />
+        <q-btn icon="question_answer" label="Chat" no-caps stack color="white" text-color="black" />
+        <q-btn icon="shopping_cart" label="Cart" no-caps stack color="white" text-color="black" />
+      </q-btn-group>
+    </q-footer>
+
+  </q-layout>
+</template>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,68 @@
+<template>
+  <q-page > 
+
+  <div class="q-pa-md">
+    <q-carousel
+      animated
+      v-model="slide"
+      navigation
+      infinite
+      :autoplay="autoplay"
+      arrows
+      :fit="mode"
+      transition-prev="slide-right"
+      transition-next="slide-left"
+      @mouseenter="autoplay = false"
+      @mouseleave="autoplay = true"
+    >
+      <q-carousel-slide :name="1" img-src="~assets/1.jpg" />
+      <q-carousel-slide :name="3" img-src="~assets/3.jpg" />
+      <q-carousel-slide :name="4" img-src="~assets/4.jpg" />
+      <q-carousel-slide :name="5" img-src="~assets/5.jpg" />
+      <q-carousel-slide :name="6" img-src="~assets/d1.jpg" />
+      <q-carousel-slide :name="7" img-src="~assets/d2.jpg" />
+      <q-carousel-slide :name="8" img-src="~assets/d3.jpg" />
+      <q-carousel-slide :name="9" img-src="~assets/d4.jpg" />
+      <q-carousel-slide :name="10" img-src="~assets/d5.jpg" />
+      <q-carousel-slide :name="11" img-src="~assets/d6.jpg" />
+      <q-carousel-slide :name="12" img-src="~assets/d7.jpg" />
+    </q-carousel>
+  </div>
+  
+  <div class="q-pa-md q-gutter-sm">
+    <q-btn
+      padding="lg"
+      color="primary"
+      icon="qr_code_scanner"
+      label="Scan" 
+      no-caps 
+      stack 
+    />
+    <q-btn
+      padding="lg"
+      color="primary"
+      icon="map"
+      label="Map" 
+      no-caps 
+      stack 
+    />
+  </div>
+
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import { ref } from 'vue'
+
+export default defineComponent({
+  setup () {
+    return {
+      slide: ref(1),
+      autoplay: ref(true)
+      
+    }
+  },
+  name: 'IndexPage'
+})
+</script>
