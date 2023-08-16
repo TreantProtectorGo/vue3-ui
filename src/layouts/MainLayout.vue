8b0ae875af1d5dac09eca4ab1fea4cb0e37c145e
TreantProtectorGo <wing.ws.lee@gmail.com>
2023-08-16 09:53:02 +0800

    1

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,16 +1,34 @@
 <template>
-  <q-layout view="hHh lpR fFf" >
+  <q-layout view="hHh lpR fFf">
     <q-page-container>
       <router-view />
     </q-page-container>
-  
-    <q-footer >
-      <q-btn-group spread >
-        <q-btn icon="home" label="Home" no-caps stack color="white" text-color="black" />
-        <q-btn icon="question_answer" label="Chat" no-caps stack color="white" text-color="black" />
+
+    <q-footer>
+      <q-btn-group spread>
+        <q-btn to="/" @click="toggleHomeIcon" :icon="homeIcon" label="Home" no-caps stack color="white" text-color="black" />
+        <q-btn to="/ChatPage" icon="question_answer" label="Chat" no-caps stack color="white" text-color="black" />
         <q-btn icon="shopping_cart" label="Cart" no-caps stack color="white" text-color="black" />
       </q-btn-group>
     </q-footer>
-
   </q-layout>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      homeIcon: 'home'
+    }
+  },
+  methods: {
+    toggleHomeIcon () {
+      if (this.homeIcon === 'home') {
+        this.homeIcon = 'home'
+      } else {
+        this.homeIcon = 'home'
+      }
+    }
+  }
+}
+</script>
---
+++ src/pages/ChatPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="q-pa-md row justify-center">
+      <div style="width: 100%; max-width: 400px">
+      <q-chat-message
+          name="me"
+          :text="['hey, how are you?']"
+          sent
+      />
+      <q-chat-message
+          name="Jane"
+          :text="['doing fine, how r you?']"
+      />
+      </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import { ref } from 'vue'
+
+export default defineComponent({
+  setup () {
+    return {
+
+    }
+  },
+  name: 'ChatPage'
+})
+</script>
